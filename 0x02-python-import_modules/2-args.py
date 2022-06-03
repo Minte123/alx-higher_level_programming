453472e17a826c102275bb1093c413babfe78544
Minte123 <netacademy1986minte@yahoo.com>
2022-06-03 10:11:45 +0300

    Python Module and Import

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
     import sys
     total = len(sys.argv)
@@ -11,5 +12,3 @@ if __name__ == "__main__":
             print("{:d} arguments:".format(total - 1))
         for i in range(1, total):
             print("{:d}: {}".format(i, sys.argv[i]))
-
-
