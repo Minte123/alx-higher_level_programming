6d6083b40802a9784676d2585922ad21e83c2702
Minte123 <netacademy1986minte@yahoo.com>
2022-06-05 15:37:47 +0300

    Python Data Structures

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+# 0-print_list_integer.py
 
-def main(my_list=[]):
-    my_list=[i]
-for i in range(6):
-        print("{:d}".format(i))
-main()
+
+def print_list_integer(my_list=[]):
+    """Print all integers of a list."""
+    for i in range(len(my_list)):
+        print("{:d}".format(my_list[i]))
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
+# 9-max_integer.py
+
+
 def max_integer(my_list=[]):
-    if my_list is None or len(my_list) is 0:
-        return None
-    max = my_list[0]
+    """Find the biggest integer of a list."""
+    if len(my_list) == 0:
+        return (None)
+
+    big = my_list[0]
     for i in range(len(my_list)):
-        if max < my_list[i]:
-            max = my_list[i]
-    return max
+        if my_list[i] > big:
+            big = my_list[i]
+
+    return (big)
