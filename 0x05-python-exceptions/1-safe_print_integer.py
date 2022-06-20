7ad7e11b1f814385a9537bda1844af9c1828d3e5
Minte123 <netacademy1986minte@yahoo.com>
2022-06-20 18:21:28 +0300

    Python exception

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
+    """The function print integer type only"""
     try:
         print("{:d}".format(value))
         return True
-    except:
+    except(TypeError, ValueError):
         return False
