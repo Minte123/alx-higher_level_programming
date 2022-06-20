0a56f2d109d1a0c5eef98ff42e7ed7ee4c185cf6
Minte123 <netacademy1986minte@yahoo.com>
2022-06-20 16:48:07 +0300

    Python exception

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,12 +1,12 @@
-#!/usr/bin/python3
 def magic_calculation(a, b):
-    result = 0
+    var = 0
     for i in range(1, 3):
         try:
-            if i > a:
-                raise Exception("Too far")
-            result += (a ** b) / i
+            if (i > a):
+                raise Exception('Too far')
+            else:
+                var += (a**b)/i
         except:
-            result = b + a
+            var = (b + a)
             break
-    return result
+    return var
--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,30 +1,11 @@
 #!/usr/bin/python3
-
-
 def safe_print_list_integers(my_list=[], x=0):
-    i = 0
-    count = 0
-    for i in range(x):
+    val = 0
+    for i in range(0, x):
         try:
-            print('{:d}'.format(my_list[i]), end='')
-            count += 1
-        except ValueError:
-            pass
-        except TypeError:
-            pass
-
+            print("{:d}".format(my_list[i]), end="")
+            val += 1
+        except (ValueError, TypeError):
+            continue
     print()
-    return count
-
-if __name__ == '__main__':
-    my_list = [1, 2, 3, 4, 5]
-
-    nb_print = safe_print_list_integers(my_list, 2)
-    print("nb_print: {:d}".format(nb_print))
-
-    my_list = [1, 2, 3, "Holberton", 4, 5, [1, 2, 3]]
-    nb_print = safe_print_list_integers(my_list, len(my_list))
-    print("nb_print: {:d}".format(nb_print))
-
-    nb_print = safe_print_list_integers(my_list, len(my_list) + 2)
-    print("nb_print: {:d}".format(nb_print))
+    return val
